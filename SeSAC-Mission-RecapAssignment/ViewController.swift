859c4f91993a664c2baa5c202ef60558419e3e19
Taeyoung Won <hexter1994@gmail.com>
2024-01-18 15:44:47 +0900

    Initial Commit

---
+++ SeSAC-Mission-RecapAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Mission-RecapAssignment
+//
+//  Created by 원태영 on 1/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
