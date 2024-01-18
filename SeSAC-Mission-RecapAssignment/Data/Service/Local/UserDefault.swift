268d3f948e719ff179f41136d55388ab43ebd248
Taeyoung Won <hexter1994@gmail.com>
2024-01-19 00:05:35 +0900

    [Feat] UserDefault 관리 프로퍼티래퍼 구현

---
+++ SeSAC-Mission-RecapAssignment/Data/Service/Local/UserDefault.swift
@@ -0,0 +1,56 @@
+//
+//  UserDefault.swift
+//  SeSAC-Mission-RecapAssignment
+//
+//  Created by 원태영 on 1/18/24.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T: Codable> {
+  private let key: Key
+  private let defaultValue: T
+  private let userDefault: UserDefaults
+  
+  init(
+    key: Key,
+    defaultValue: T,
+    userDefault: UserDefaults = .standard
+  ) {
+    self.key = key
+    self.defaultValue = defaultValue
+    self.userDefault = userDefault
+  }
+  
+  var wrappedValue: T {
+    get {
+      guard 
+        let data = userDefault.data(forKey: key.name),
+        let value = try? JsonCoder.decoder.decode(T.self, from: data)
+      else {
+        return defaultValue
+      }
+      
+      return value
+    }
+    set {
+      let data: Data? = try? JsonCoder.encoder.encode(newValue)
+      
+      UserDefaults.standard.setValue(data, forKey: key.name)
+    }
+  }
+}
+
+extension UserDefault {
+  enum Key: String {
+    case nickname
+    case profile
+    case likes
+    case recentSearches
+    
+    var name: String {
+      return self.rawValue
+    }
+  }
+}
---
+++ SeSAC-Mission-RecapAssignment/Utility/Manager/JsonCoder.swift
@@ -0,0 +1,13 @@
+//
+//  JsonCoder.swift
+//  SeSAC-Mission-RecapAssignment
+//
+//  Created by 원태영 on 1/18/24.
+//
+
+import Foundation
+
+enum JsonCoder {
+  static let encoder: JSONEncoder = JSONEncoder()
+  static let decoder: JSONDecoder = JSONDecoder()
+}
