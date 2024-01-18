cb3addd770b0a75b3ee4da5db9b2a8751a642390
Taeyoung Won <hexter1994@gmail.com>
2024-01-18 19:47:09 +0900

    [Feat] RAError 프로토콜 추가요소 정의, 에러 로그 및 reThrow 로직 구현

--- SeSAC-Mission-RecapAssignment/App/RAError.swift
+++ SeSAC-Mission-RecapAssignment/App/RAError.swift
@@ -6,6 +6,19 @@
 //
 
 protocol RAError: Error {
-  var logDescrption: String { get }
+  var logDescription: String { get }
   var alertDescription: String { get }
+  func log(from function: String)
+  func reThrow(from function: String) -> Self
+}
+
+extension RAError {
+  func log(from function: String) {
+    print("[에러 발생]", function, logDescription, separator: "\n")
+  }
+  
+  func reThrow(from function: String) -> Self {
+    log(from: function)
+    return self
+  }
 }
