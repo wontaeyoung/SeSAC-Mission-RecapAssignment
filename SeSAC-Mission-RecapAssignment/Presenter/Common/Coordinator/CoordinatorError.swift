b49a108fcadd8286161aad7afb378aa0896a7ce8
Taeyoung Won <hexter1994@gmail.com>
2024-01-19 15:59:55 +0900

    [Feat] Coordinator Error 열거형 정의

---
+++ SeSAC-Mission-RecapAssignment/Presenter/Common/Coordinator/CoordinatorError.swift
@@ -0,0 +1,22 @@
+//
+//  CoordinatorError.swift
+//  SeSAC-Mission-RecapAssignment
+//
+//  Created by 원태영 on 1/19/24.
+//
+
+enum CoordinatorError: RAError {
+  case undefiendError
+  
+  var logDescription: String {
+    switch self {
+      case .undefiendError:
+        return "정의되지 않은 오류 발생"
+    }
+  }
+  
+  var alertDescription: (title: String, message: String) {
+    return (title: "오류 발생",
+            message: .combineWithLineBreaks("알 수 없는 오류가 발생했어요.", "잠시 후 다시 시도해주세요."))
+  }
+}
