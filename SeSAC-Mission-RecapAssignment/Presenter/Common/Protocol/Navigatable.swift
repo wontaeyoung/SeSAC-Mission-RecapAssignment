b447b99f6cc8ca89d6a06abef0df58dcb422f0a6
Taeyoung Won <hexter1994@gmail.com>
2024-01-20 00:01:31 +0900

    [Del] Navigatable 프로토콜에서 데이터 세팅 강제하지 않도록 수정

--- SeSAC-Mission-RecapAssignment/Presenter/Common/Protocol/Navigatable.swift
+++ SeSAC-Mission-RecapAssignment/Presenter/Common/Protocol/Navigatable.swift
@@ -9,9 +9,5 @@ import UIKit
 
 protocol Navigatable: UIViewController {
   
-  associatedtype Entity: RAModel
-  
   static var identifier: String { get }
-  
-  func setData(data: Entity, bindAction: (() -> Void)?)
 }
