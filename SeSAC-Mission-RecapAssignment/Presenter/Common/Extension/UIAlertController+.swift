1cdb11260d7a3dca11fbbbba6c5503f32e657026
Taeyoung Won <hexter1994@gmail.com>
2024-01-19 23:09:55 +0900

    [Feat] UIAlertController 익스텐션 메서드 체이닝 방식의 action 추가 함수 구현

--- SeSAC-Mission-RecapAssignment/Presenter/Common/Extension/UIAlertController+.swift
+++ SeSAC-Mission-RecapAssignment/Presenter/Common/Extension/UIAlertController+.swift
@@ -11,4 +11,22 @@ extension UIAlertController {
   func addActions(_ actions: UIAlertAction...) {
     actions.forEach { self.addAction($0) }
   }
+  
+  func setAction(
+    title: String?,
+    style: UIAlertAction.Style = .default,
+    completion: (() -> Void)? = nil
+  ) -> UIAlertController {
+    let alertAction = UIAlertAction(title: title, style: style) { _ in
+      completion?()
+    }
+    
+    addAction(alertAction)
+    
+    return self
+  }
+  
+  func setCancelAction() -> UIAlertController {
+    return setAction(title: "취소", style: .cancel)
+  }
 }
