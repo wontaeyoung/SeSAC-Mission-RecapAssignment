6b6c62aa9643e9bea1e1c48b690601ccf5fba5af
Taeyoung Won <hexter1994@gmail.com>
2024-01-19 15:09:50 +0900

    [Add] 전체 프로필 이미지 리스트 추가

--- SeSAC-Mission-RecapAssignment/Utility/Constant/Constant.swift
+++ SeSAC-Mission-RecapAssignment/Utility/Constant/Constant.swift
@@ -5,11 +5,17 @@
 //  Created by 원태영 on 1/18/24.
 //
 
-import Foundation
+import UIKit
 
 enum Constant {
   enum Network {
     static var requestTimeLimit: Double { return 10 }
     static var resourceTimeLimit: Double { return 15 }
+  }
+  
+  enum Profile {
+    static let profileList: [UIImage] = [.profile1, .profile2, .profile3, .profile4, .profile5,
+                                         .profile6, .profile7, .profile8, .profile9, .profile10,
+                                         .profile11, .profile12, .profile13, .profile14]
   }
 }
