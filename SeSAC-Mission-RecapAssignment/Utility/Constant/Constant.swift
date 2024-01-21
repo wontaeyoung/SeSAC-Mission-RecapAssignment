1976708a9543cf1075cad532ad4516120865b112
Taeyoung Won <hexter1994@gmail.com>
2024-01-21 10:36:38 +0900

    [Del] 미사용 이미지 리스트 상수 삭제

--- SeSAC-Mission-RecapAssignment/Utility/Constant/Constant.swift
+++ SeSAC-Mission-RecapAssignment/Utility/Constant/Constant.swift
@@ -14,12 +14,6 @@ enum Constant {
     static var resourceTimeLimit: Double { return 15 }
   }
   
-  enum Profile {
-    static let profileList: [UIImage] = [.profile1, .profile2, .profile3, .profile4, .profile5,
-                                         .profile6, .profile7, .profile8, .profile9, .profile10,
-                                         .profile11, .profile12, .profile13, .profile14]
-  }
-  
   enum Storyboard: String {
     case Onboarding
     case ProfileSetting
