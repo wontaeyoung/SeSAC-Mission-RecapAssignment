6e22e1a6b82453c2c1dcf5618e4fcb2462d1bf54
Taeyoung Won <hexter1994@gmail.com>
2024-01-22 12:46:53 +0900

    [Style] 상품 컬렉션 셀 UI 구현

---
+++ SeSAC-Mission-RecapAssignment/Presenter/MainTabBar/Search/SearchResult/ProductCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ProductCollectionViewCell.swift
+//  SeSAC-Mission-RecapAssignment
+//
+//  Created by 원태영 on 1/22/24.
+//
+
+import UIKit
+import Kingfisher
+
+final class ProductCollectionViewCell: BaseCollectionViewCell {
+  
+  @IBOutlet weak var productImageView: UIImageView!
+  @IBOutlet weak var mallNameLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var likeButton: UIButton!
+  
+  override func configure() {
+    DesignSystemManager.configureProductImageView(productImageView)
+    DesignSystemManager.configureMallNameLabel(mallNameLabel)
+    DesignSystemManager.configureProductTitleLabel(titleLabel)
+    DesignSystemManager.configureProductPriceLabel(priceLabel)
+    DesignSystemManager.configureLikeButton(likeButton)
+  }
+  
+  func setData(product: Product, tag: Int) {
+    let likeImage = User.default.likes.contains(product.productID)
+    ? RADesign.Image.likeFill.image
+    : RADesign.Image.like.image
+    
+    productImageView.kf.setImage(with: product.url)
+    mallNameLabel.text = product.mallName
+    titleLabel.text = product.title
+    priceLabel.text = product.lprice.formatted
+    likeButton.setImage(likeImage, for: .normal)
+    likeButton.tag = tag
+  }
+}
