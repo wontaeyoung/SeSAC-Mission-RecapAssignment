d957446cd67620a9b66d7f6f545a24d75a44aeb2
Taeyoung Won <hexter1994@gmail.com>
2024-01-25 09:08:01 +0900

    [Fix] 알림 오프로 변경

--- SeSAC-Mission-RecapAssignment/Utility/Manager/UserNotificationManager.swift
+++ SeSAC-Mission-RecapAssignment/Utility/Manager/UserNotificationManager.swift
@@ -43,7 +43,11 @@ final class UserNotificationManager: NSObject {
       trigger: trigger.trigger
     )
     
-    notiCenter.add(request)
+    /// ON
+//    notiCenter.add(request)
+    
+    /// OFF
+    notiCenter.removeAllPendingNotificationRequests()
   }
 }
 
