6ca2c2b4fe234608b0ef015d94a4a72070466c5c
Taeyoung Won <hexter1994@gmail.com>
2024-02-23 21:00:29 +0900

    [Fix] ViewModel 비즈니스 로직 분리

--- SeSAC-Mission-RecapAssignment/Presenter/MainTabBar/Setting/SettingViewController.swift
+++ SeSAC-Mission-RecapAssignment/Presenter/MainTabBar/Setting/SettingViewController.swift
@@ -11,8 +11,7 @@ final class SettingViewController: BaseTableViewController, Navigatable, ViewMod
   
   @IBOutlet weak var settingTableView: UITableView!
   
-  private var viewModel: SettingViewModel?
-  private let settingItems: [String] = ["공지사항", "자주 묻는 질문", "1:1 문의", "알림 설정", "처음부터 시작하기"]
+  private var viewModel: SettingViewModel!
   
   override func viewWillAppear(_ animated: Bool) {
     settingTableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
@@ -34,11 +33,7 @@ final class SettingViewController: BaseTableViewController, Navigatable, ViewMod
   }
   
   func presentLikesCountInfoAlert() {
-    viewModel?.coordinator?.showAlert(
-      title: "좋아요 갯수 안내",
-      message: "현재 관심 상품 갯수는 \(User.default.likes.count) 입니다.",
-      isCancelable: false
-    )
+    viewModel.showLikeCountInfoAlert()
   }
 }
 
@@ -48,7 +43,7 @@ extension SettingViewController: TableConfigurable {
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return section == 0 ? 1 : settingItems.count
+    return section == 0 ? 1 : viewModel.settingItems.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -63,7 +58,7 @@ extension SettingViewController: TableConfigurable {
       let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath)
       
       let row: Int = indexPath.row
-      cell.textLabel?.text = settingItems[row]
+      cell.textLabel?.text = viewModel.settingItems[row]
       cell.textLabel?.textColor = .raText
       cell.textLabel?.font = RADesign.Font.captionBlack.font
       
@@ -74,15 +69,8 @@ extension SettingViewController: TableConfigurable {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     if indexPath.section == 0 {
       viewModel?.showNicknameSettingViewController()
-    } else if indexPath.row == settingItems.count - 1 {
-      
-      viewModel?.coordinator?.showAlert(
-        title: "처음부터 시작하기",
-        message: "데이터를 모두 초기화하시겠습니까?",
-        okStyle: .destructive
-      ) {
-        self.viewModel?.resetProfile()
-      }
+    } else if indexPath.row == viewModel.settingItems.count - 1 {
+      viewModel.showResetAlert()
     }
     
     tableView.reloadRows(at: [indexPath], with: .automatic)
--- SeSAC-Mission-RecapAssignment/Presenter/MainTabBar/Setting/SettingViewModel.swift
+++ SeSAC-Mission-RecapAssignment/Presenter/MainTabBar/Setting/SettingViewModel.swift
@@ -8,6 +8,8 @@
 final class SettingViewModel: ViewModel {
   weak var coordinator: SettingCoordinator?
   
+  let settingItems: [String] = ["공지사항", "자주 묻는 질문", "1:1 문의", "알림 설정", "처음부터 시작하기"]
+  
   init(coordinator: SettingCoordinator) {
     self.coordinator = coordinator
   }
@@ -24,5 +26,25 @@ final class SettingViewModel: ViewModel {
   func resetProfile() {
     User.default.resetProfile()
     coordinator?.end()
+  }
+  
+  @MainActor
+  func showResetAlert() {
+    coordinator?.showAlert(
+      title: "처음부터 시작하기",
+      message: "데이터를 모두 초기화하시겠습니까?",
+      okStyle: .destructive
+    ) {
+      self.resetProfile()
+    }
+  }
+  
+  @MainActor
+  func showLikeCountInfoAlert() {
+    coordinator?.showAlert(
+      title: "좋아요 갯수 안내",
+      message: "현재 관심 상품 갯수는 \(User.default.likes.count) 입니다.",
+      isCancelable: false
+    )
   }
 }
